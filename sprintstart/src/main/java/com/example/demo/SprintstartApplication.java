0311a3673fdbdc0b5d4c65f0844a4ac0e7d0e1d7
Dhananjaypp <Dhananjay.submit@gmail.com>
2022-02-02 18:17:53 +0530

    yyyyyyy

---
+++ sprintstart/src/main/java/com/example/demo/SprintstartApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintstartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintstartApplication.class, args);
+	}
+
+}
